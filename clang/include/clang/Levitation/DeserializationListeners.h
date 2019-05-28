a4d27892cff51e1c4fe9443cd5968aac9360e6a1
Stepan Dyatkovskiy <ml@dyatkovskiy.com>
2019-05-28 22:17:55 +0400

    C++ Levitation: forgot to add DeserializationListeners.h is required for preamble support fix commit.

---
+++ clang/include/clang/Levitation/DeserializationListeners.h
@@ -0,0 +1,112 @@
+// TODO Levitation: Licensing
+
+#ifndef LLVM_CLANG_FRONTEND_LEVITATION_DESERIALIZATIONLISTENERS_H
+#define LLVM_CLANG_FRONTEND_LEVITATION_DESERIALIZATIONLISTENERS_H
+
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/Decl.h"
+#include "clang/Serialization/ASTDeserializationListener.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <set>
+
+namespace clang { namespace levitation {
+
+// Below are just clones of ones defined in FrontendAction.cpp
+
+class DelegatingDeserializationListener : public ASTDeserializationListener {
+  ASTDeserializationListener *Previous;
+  bool DeletePrevious;
+
+public:
+  explicit DelegatingDeserializationListener(
+      ASTDeserializationListener *Previous, bool DeletePrevious)
+      : Previous(Previous), DeletePrevious(DeletePrevious) {}
+  ~DelegatingDeserializationListener() override {
+    if (DeletePrevious)
+      delete Previous;
+  }
+
+  void ReaderInitialized(ASTReader *Reader) override {
+    if (Previous)
+      Previous->ReaderInitialized(Reader);
+  }
+  void IdentifierRead(serialization::IdentID ID,
+                      IdentifierInfo *II) override {
+    if (Previous)
+      Previous->IdentifierRead(ID, II);
+  }
+  void TypeRead(serialization::TypeIdx Idx, QualType T) override {
+    if (Previous)
+      Previous->TypeRead(Idx, T);
+  }
+  void DeclRead(serialization::DeclID ID, const Decl *D) override {
+    if (Previous)
+      Previous->DeclRead(ID, D);
+  }
+  void SelectorRead(serialization::SelectorID ID, Selector Sel) override {
+    if (Previous)
+      Previous->SelectorRead(ID, Sel);
+  }
+  void MacroDefinitionRead(serialization::PreprocessedEntityID PPID,
+                           MacroDefinitionRecord *MD) override {
+    if (Previous)
+      Previous->MacroDefinitionRead(PPID, MD);
+  }
+};
+
+/// Dumps deserialized declarations.
+class DeserializedDeclsDumper : public DelegatingDeserializationListener {
+public:
+  explicit DeserializedDeclsDumper(ASTDeserializationListener *Previous,
+                                   bool DeletePrevious)
+      : DelegatingDeserializationListener(Previous, DeletePrevious) {}
+
+  void DeclRead(serialization::DeclID ID, const Decl *D) override {
+
+    llvm::outs() << "PCH DECL, ID = " << ID << ": "
+                 << D->getDeclKindName();
+
+    if (const NamedDecl *ND = dyn_cast<NamedDecl>(D)) {
+      llvm::outs() << " - ";
+      ND->printQualifiedName(llvm::outs());
+    }
+    llvm::outs() << "\n";
+
+    DelegatingDeserializationListener::DeclRead(ID, D);
+  }
+};
+
+/// Checks deserialized declarations and emits error if a name
+/// matches one given in command-line using -error-on-deserialized-decl.
+class DeserializedDeclsChecker : public DelegatingDeserializationListener {
+  ASTContext &Ctx;
+  std::set<std::string> NamesToCheck;
+
+public:
+  DeserializedDeclsChecker(ASTContext &Ctx,
+                           const std::set<std::string> &NamesToCheck,
+                           ASTDeserializationListener *Previous,
+                           bool DeletePrevious)
+      : DelegatingDeserializationListener(Previous, DeletePrevious), Ctx(Ctx),
+        NamesToCheck(NamesToCheck) {}
+
+  void DeclRead(serialization::DeclID ID, const Decl *D) override {
+    if (const auto *ND = dyn_cast<NamedDecl>(D))
+      if (NamesToCheck.find(ND->getNameAsString()) != NamesToCheck.end()) {
+        unsigned DiagID
+          = Ctx.getDiagnostics().getCustomDiagID(DiagnosticsEngine::Error,
+                                                 "%0 was deserialized");
+        Ctx.getDiagnostics().Report(Ctx.getFullLoc(D->getLocation()), DiagID)
+            << ND->getNameAsString();
+      }
+
+    DelegatingDeserializationListener::DeclRead(ID, D);
+  }
+};
+
+}} // end of clang::levitation
+
+#endif // #ifndef LLVM_CLANG_FRONTEND_LEVITATION_DESERIALIZATIONLISTENERS_H
+
