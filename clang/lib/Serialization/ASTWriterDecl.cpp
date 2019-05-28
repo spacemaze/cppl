adca903529ca87c7bd6dbd4a1338e5b4d12ed54b
Stepan Dyatkovskiy <ml@dyatkovskiy.com>
2019-05-28 22:18:43 +0400

    C++ Levitation, NamespaceDecl: Fixed package namespaces loading.

--- clang/lib/Serialization/ASTWriterDecl.cpp
+++ clang/lib/Serialization/ASTWriterDecl.cpp
@@ -1185,6 +1185,7 @@ void ASTDeclWriter::VisitNamespaceDecl(NamespaceDecl *D) {
   VisitRedeclarable(D);
   VisitNamedDecl(D);
   Record.push_back(D->isInline());
+  Record.push_back(D->isLevitationPackage());
   Record.AddSourceLocation(D->getBeginLoc());
   Record.AddSourceLocation(D->getRBraceLoc());
 
