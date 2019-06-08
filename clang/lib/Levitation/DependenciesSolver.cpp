ce863afd56d5d521be1e9b4a6e81a5c0d919ad7c
Stepan Dyatkovskiy <ml@dyatkovskiy.com>
2019-06-08 23:39:43 +0400

    C++ Levitation, DependenciesSolver: implemented dependencies solving itself.

--- clang/lib/Levitation/DependenciesSolver.cpp
+++ clang/lib/Levitation/DependenciesSolver.cpp
@@ -16,6 +16,8 @@
 
 #include "llvm/Support/raw_ostream.h"
 
+#include <algorithm>
+
 using namespace llvm;
 
 namespace clang { namespace levitation {
@@ -109,7 +111,7 @@ public:
 // Dependencies DAG
 
 class DependenciesDAG {
-
+public:
   struct Node;
 
   struct PackageInfo {
@@ -162,6 +164,8 @@ class DependenciesDAG {
     NodesSet DependentNodes;
   };
 
+private:
+
   /// Nodes whithout dependencies.
   NodesSet Roots;
 
@@ -205,58 +209,62 @@ public:
     return DAGPtr;
   }
 
+  // TODO Levitation: That looks pretty much like A* walk.
+  //
+  void bsfWalkSkipVisited(std::function<void(const Node &)> OnNode) const {
+    bsfWalk(true, OnNode);
+  }
+
+  void bsfWalk(std::function<void(const Node &)> OnNode) const {
+    bsfWalk(false, OnNode);
+  }
+
   // Do BFS graph walk and dump each node
   void dump(
       llvm::raw_ostream &out, const DependenciesStringsPool &Strings
-  ) {
-    NodesSet Worklist = Roots;
-
-    NodesSet VisitedNodes;
-    NodesSet NewWorklist;
-
-    while (Worklist.size()) {
-      NewWorklist.clear();
-      for (auto NodeID : Worklist) {
-        auto &Node = *AllNodes[NodeID];
-
-        if (!VisitedNodes.insert(NodeID).second)
-          continue;
-
-        out
-        << "Node[";
-        dump(out, NodeID);
-        out << "]\n"
-        << "    Path: " << *Strings.getItem(Node.PackageInfo->PackagePath) << "\n"
-        << "    Kind: "
-        << (Node.Kind == NodeKind::Declaration ? "Declaration" : "Definition") << "\n";
-
-        if (Node.DependentNodes.size()) {
-          out << "    Is used by:\n";
-          for (auto DependentNodeID : Node.DependentNodes) {
-            out << "        ";
-            dump(out, DependentNodeID);
-            out << "\n";
-          }
-        }
-
-        if (Node.Dependencies.size()) {
-          out << "    Dependencies:\n";
-          for (auto DependencyID : Node.Dependencies) {
-            out << "        ";
-            dump(out, DependencyID);
-            out << "\n";
-          }
-        }
-
+  ) const {
+    bsfWalkSkipVisited([&](const Node &N) {
+        dumpNode(out, N.ID, Strings);
         out << "\n";
+    });
+  }
 
-        NewWorklist.insert(Node.DependentNodes.begin(), Node.DependentNodes.end());
+  void dumpNode(
+      llvm::raw_ostream &out,
+      NodeID::Type NodeID,
+      const DependenciesStringsPool &Strings
+  ) const {
+
+    const Node &Node = getNode(NodeID);
+
+    out
+    << "Node[";
+    dumpNodeID(out, NodeID);
+    out << "]\n"
+    << "    Path: " << *Strings.getItem(Node.PackageInfo->PackagePath) << "\n"
+    << "    Kind: "
+    << (Node.Kind == NodeKind::Declaration ? "Declaration" : "Definition") << "\n";
+
+    if (Node.DependentNodes.size()) {
+      out << "    Is used by:\n";
+      for (auto DependentNodeID : Node.DependentNodes) {
+        out << "        ";
+        dumpNodeID(out, DependentNodeID);
+        out << "\n";
       }
-      Worklist.swap(NewWorklist);
+    }
+
+    if (Node.Dependencies.size()) {
+      out << "    Dependencies:\n";
+      for (auto DependencyID : Node.Dependencies) {
+        out << "        ";
+        dumpNodeID(out, DependencyID);
+        out << "\n";
+      }
     }
   }
 
-  void dump(llvm::raw_ostream &out, NodeID::Type NodeID) {
+  void dumpNodeID(llvm::raw_ostream &out, NodeID::Type NodeID) const {
     NodeKind Kind;
     StringID PathID;
     std::tie(Kind, PathID) = NodeID::getKindAndPathID(NodeID);
@@ -266,10 +274,45 @@ public:
     << (Kind == NodeKind::Declaration ? "DECL" : "DEF");
   }
 
+  const Node &getNode(NodeID::Type ID) const {
+    auto Found = AllNodes.find(ID);
+    assert(
+        Found != AllNodes.end() &&
+        "Node with current ID should be present in AllNodes"
+    );
+    return *Found->second;
+  }
+
   const NodesMap &allNodes() const { return AllNodes; }
   const NodesSet &roots() const { return Roots; }
 
 protected:
+
+  void bsfWalk(
+      bool SkipVisited,
+      std::function<void(const Node&)> OnNode
+  ) const {
+    NodesSet Worklist = Roots;
+
+    NodesSet VisitedNodes;
+    NodesSet NewWorklist;
+
+    while (Worklist.size()) {
+      NewWorklist.clear();
+      for (auto NID : Worklist) {
+
+        if (SkipVisited && !VisitedNodes.insert(NID).second)
+          continue;
+
+        const auto &Node = getNode(NID);
+
+        OnNode(Node);
+
+        NewWorklist.insert(Node.DependentNodes.begin(), Node.DependentNodes.end());
+      }
+      Worklist.swap(NewWorklist);
+    }
+  }
 
   void addDependenciesTo(
       Node &DependentNode,
@@ -334,20 +377,204 @@ protected:
 // Dependencies solving
 
 class SolvedDependenciesInfo {
+
+  using NodeID = DependenciesDAG::NodeID;
+  using Node = DependenciesDAG::Node;
+
+  struct DependencyWithDistance {
+    NodeID::Type NodeID;
+    int Range;
+  };
+
+  using FullDependencies =
+      DenseMap<NodeID::Type, std::unique_ptr<DependencyWithDistance>>;
+
+  using FullDependenciesMap =
+      DenseMap<NodeID::Type, std::unique_ptr<FullDependencies>>;
+
+  using FullDependenciesList = SmallVector<DependencyWithDistance, 16>;
+
+  using FullDependenciesSortedMap =
+      DenseMap<NodeID::Type, std::unique_ptr<FullDependenciesList>>;
+
+  const DependenciesDAG &DDAG;
+  FullDependenciesMap FullDepsMap;
+  FullDependenciesSortedMap FullDepsSortedMap;
+
+  SolvedDependenciesInfo(const DependenciesDAG &DDAG) : DDAG(DDAG) {}
+
 public:
+
+
+  static SolvedDependenciesInfo build(
+      const DependenciesDAG &DDAG,
+      const DependenciesStringsPool &Strings,
+      llvm::raw_ostream &errs
+  ) {
+
+    SolvedDependenciesInfo SolvedInfo(DDAG);
+
+    FullDependencies EmptyList;
+
+    DDAG.bsfWalk([&] (const Node &N) {
+      NodeID::Type NID = N.ID;
+
+      const auto &CurrentDependencies = N.Dependencies.size() ?
+          SolvedInfo.getDependencies(NID) :
+          EmptyList;
+
+      for (auto DependentNodeID : N.DependentNodes) {
+        auto &NextDependencies =
+            SolvedInfo.getOrCreateDependencies(DependentNodeID);
+
+        SolvedInfo.merge(NextDependencies, CurrentDependencies, NID);
+      }
+    });
+
+    SolvedInfo.sortAllDependencies();
+
+    return SolvedInfo;
+  }
+
+  const FullDependencies &getDependencies(NodeID::Type NID) const {
+    auto Found = FullDepsMap.find(NID);
+    assert(
+        Found != FullDepsMap.end() &&
+        "Deps list should be present in FullDepsMap"
+    );
+
+    return *Found->second;
+  }
+
+  const FullDependenciesList &getDependenciesList(NodeID::Type NID) const {
+    static FullDependenciesList EmptyList;
+
+    auto Found = FullDepsSortedMap.find(NID);
+
+    if (Found == FullDepsSortedMap.end())
+      return EmptyList;
+
+    return *Found->second;
+  }
+
+  void dump(
+      llvm::raw_ostream &out,
+      const DependenciesStringsPool &Strings
+  ) const {
+    DDAG.bsfWalkSkipVisited([&](const Node &N) {
+      auto NID = N.ID;
+      const FullDependenciesList &FullDeps = getDependenciesList(NID);
+
+      const auto &Path = *Strings.getItem(N.PackageInfo->PackagePath);
+      out << "[";
+      DDAG.dumpNodeID(out, NID);
+      out << "]\n";
+
+      out << "    Path: " << Path << "\n";
+      if (FullDeps.size()) {
+        out
+                << "    Full dependencies:\n";
+
+        for (const auto &DepWithDist : FullDeps) {
+          const auto &Dep = DDAG.getNode(DepWithDist.NodeID);
+          const auto &DepPath = *Strings.getItem(Dep.PackageInfo->PackagePath);
+
+          out.indent(8) << "[";
+          DDAG.dumpNodeID(out, DepWithDist.NodeID);
+          out
+                  << "]: " << DepPath << "\n";
+        }
+
+        out
+                << "    Direct dependencies:\n";
+
+        for (const auto &DepID : N.Dependencies) {
+          const auto &Dep = DDAG.getNode(DepID);
+          const auto &DepPath = *Strings.getItem(Dep.PackageInfo->PackagePath);
+
+          out
+                  << "        " << DepPath << "\n";
+        }
+      } else {
+        out << "    (root)\n";
+      }
+    });
+  }
+
+protected:
+
+  FullDependencies &getOrCreateDependencies(NodeID::Type NID) {
+    auto ListRes = FullDepsMap.insert({ NID, nullptr });
+
+    if (ListRes.second)
+      ListRes.first->second = llvm::make_unique<FullDependencies>();
+
+    return *ListRes.first->second;
+  }
+
+  void merge(
+      FullDependencies &DestList,
+      const FullDependencies &PrevList,
+      NodeID::Type DirectDependencyNodeID
+  ) {
+    for (const auto &DepWithDistIt : PrevList) {
+      const auto &DepWithDist = *DepWithDistIt.second;
+
+      insertOrUpdDepWithDistance(DestList, DepWithDist.NodeID, DepWithDist.Range + 1);
+    }
+
+    insertOrUpdDepWithDistance(DestList, DirectDependencyNodeID, 1);
+  }
+
+  void insertOrUpdDepWithDistance(
+          FullDependencies &DestList,
+          NodeID::Type NID,
+          int Range
+  ) {
+      auto InsertionRes = DestList.insert( {NID, nullptr} );
+
+      if (InsertionRes.second) {
+        InsertionRes.first->second = llvm::make_unique<DependencyWithDistance>(
+            DependencyWithDistance {NID, Range}
+        );
+      } else {
+        auto &ExistingList = *InsertionRes.first->second;
+        ExistingList.Range = std::max(ExistingList.Range, Range);
+      }
+  }
+
+  void sortAllDependencies() {
+    for (auto &NodeIt : FullDepsMap) {
+      auto Res = FullDepsSortedMap.insert({
+        NodeIt.first,
+        llvm::make_unique<FullDependenciesList>()
+      });
+
+      FullDependencies &NodeDeps = *NodeIt.second;
+      FullDependenciesList &List = *Res.first->second;
+      sortNodeDependencies(List, NodeDeps);
+    }
+  }
+
+  void sortNodeDependencies(
+      FullDependenciesList &Dest, const FullDependencies &Src
+  ) {
+    for (const auto &DepWithDistIt : Src) {
+      const DependencyWithDistance &Dep = *DepWithDistIt.second;
+      Dest.push_back(Dep);
+    }
+
+    std::sort(
+        Dest.begin(),
+        Dest.end(),
+        [=](const DependencyWithDistance &L, const DependencyWithDistance &R) {
+            return L.Range > R.Range;
+        }
+    );
+  }
 };
 
 using SolvedDependenciesMap = llvm::DenseMap<llvm::StringRef, std::unique_ptr<SolvedDependenciesInfo>>;
-
-static std::unique_ptr<SolvedDependenciesMap> buildSolvedDependencies(
-    const DependenciesDAG &DDAG,
-    llvm::raw_ostream &out
-) {
-  with (auto A = DumpAction(out, "Solving dependencies")) {
-  }
-
-  return nullptr;
-}
 
 using Paths = llvm::SmallVector<llvm::SmallString<256>, 64>;
 using ParsedDependenciesVector = Paths;
@@ -500,7 +727,7 @@ public:
   }
 
   void writeResult(
-      const SolvedDependenciesMap &SolvedDependencies
+      const SolvedDependenciesInfo &SolvedDependencies
   ) {
     with (auto A = DumpAction(Solver->verbose(), "Generating dependency files")) {
 
@@ -527,15 +754,27 @@ void DependenciesSolver::solve() {
   ParsedDependencies parsedDependencies;
   Helper.loadDependencies(parsedDependencies, ParsedDepFiles);
 
+  verbose()
+  << "Loaded dependencies:\n";
   Helper.dump(parsedDependencies);
 
   auto DDAG = DependenciesDAG::build(parsedDependencies, verbose());
+
+  verbose()
+  << "Dependencies DAG:\n";
   DDAG->dump(verbose(), parsedDependencies.getStringsPool());
 
-  auto SolvedDependencies = buildSolvedDependencies(*DDAG, verbose());
+  auto SolvedInfo = SolvedDependenciesInfo::build(
+      *DDAG,
+      parsedDependencies.getStringsPool(),
+      error()
+  );
 
-  Helper.writeResult(*SolvedDependencies);
+  verbose()
+  << "Dependencies solved info:\n";
+  SolvedInfo.dump(verbose(), parsedDependencies.getStringsPool());
 
+  Helper.writeResult(SolvedInfo);
 }
 
 llvm::raw_ostream &DependenciesSolver::verbose() {
